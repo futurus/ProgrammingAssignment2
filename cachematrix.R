423a43631add779a322bc9589c8b955888299407
Vu Nguyen <futurus@live.com>
2014-08-06 14:11:12 -0400

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,9 +6,9 @@ makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   chg <- FALSE
   set <- function(y) {
-    if (identical(x, y)) {
+    if (identical(x, y)) { ## if x and y are identical, skip
       return;
-    } else {
+    } else {  ## otherwise set x to new matrix y and declare that it has changed
       x <<- y
       chg <<- TRUE
       m <<- NULL
@@ -30,7 +30,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## if inverse is calculated AND x hasn't changed, show the inverse
 ## otherwise, calculate the inverse and set x's change status to false 
-## (it hasn't change since last calculation)
+## (it hasn't changed since last calculation)
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
@@ -42,6 +42,6 @@ cacheSolve <- function(x, ...) {
   
   m <- solve(x$get(), ...)
   x$sInverse(m)
-  x$sChange()
+  x$sChange() ## reset x's change status to FALSE (since last calculation, which is just now
   m
 }
