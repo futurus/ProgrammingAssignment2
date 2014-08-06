c90ae3c43ca42419f2d2eef6611f274829acc4a5
Vu Nguyen <futurus@live.com>
2014-08-06 14:00:14 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions support cached matrix inversion
 
-## Write a short comment describing this function
+## this function mirrors the example, additionally it keeps track matrix x's change status
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  chg <- FALSE
+  set <- function(y) {
+    if (identical(x, y)) {
+      return;
+    } else {
+      x <<- y
+      chg <<- TRUE
+      m <<- NULL
+    }
+  }
+  
+  get <- function() x
+  sInverse <- function(inv) m <<- inv
+  gInverse <- function() m
+  sChange <- function() chg <<- FALSE
+  change <- function() chg
+  list(set = set, get = get,
+       sInverse = sInverse,
+       gInverse = gInverse,
+       sChange = sChange,
+       change = change)
 }
 
 
-## Write a short comment describing this function
+## if inverse is calculated AND x hasn't changed, show the inverse
+## otherwise, calculate the inverse and set x's change status to false 
+## (it hasn't change since last calculation)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$gInverse()
+  if(!is.null(m) & !x$change()) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  m <- solve(x$get(), ...)
+  x$sInverse(m)
+  x$sChange()
+  m
 }
